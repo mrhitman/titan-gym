d41dd4eeed3d415564806d160132aaebc0b3ca1a
hitman <hitman@quartsoft.com>
2017-11-21 16:58:30 +0200

    add comments

--- src/routes.php
+++ src/routes.php
@@ -6,8 +6,7 @@ use src\AdminController;
 use Xandros15\SlimPagination\Pagination;
 
 $app->get('/', function (Request $request, Response $response, array $args) {
-    $db = $this->db;
-    /* @var $db Illuminate\Database\Capsule\Manager */
+    $db = $this->db; /* @var $db Illuminate\Database\Capsule\Manager */
 
     $config = $db
         ->table('config')
@@ -20,8 +19,33 @@ $app->get('/', function (Request $request, Response $response, array $args) {
         'pagination' => new Pagination($request, $this->get('router'), [
             Pagination::OPT_TOTAL => $db->table('comment')->count(),
         ]),
-        'comments' => $db->table('comment')->limit(10),
+        'comments' => $db->table('comment')->where(['published' => 1])->limit(10)->get(),
     ]);
+});
+
+$app->group('/comment', function () {
+
+    $this->get('/{page}', function (Request $request, Response $response, array $args) {
+        $db = $this->db; /* @var $db Illuminate\Database\Capsule\Manager */
+        $comments = $db->table('comment')->get();
+        var_dump($comments);
+    });
+
+    $this->get('/like/{id}', function (Request $request, Response $response, array $args) {
+        $db = $this->db; /* @var $db Illuminate\Database\Capsule\Manager */
+        $comment = $db->table('comment')->find($args['id']);
+        $comment->likes++;
+        $db->table('comment')->where(['id' => $comment->id])->update((array) $comment);
+        return $response->withRedirect('/');
+    });
+
+    $this->get('/dislike/{id}', function (Request $request, Response $response, array $args) {
+        $db = $this->db; /* @var $db Illuminate\Database\Capsule\Manager */
+        $comment = $db->table('comment')->find($args['id']);
+        $comment->likes--;
+        $db->table('comment')->where(['id' => $comment->id])->update((array) $comment);
+        return $response->withRedirect('/');
+    });
 });
 
 $app->group('/admin', function () {
--- titan-gym.sql
+++ titan-gym.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50720
  File Encoding         : 65001
 
- Date: 17/11/2017 15:57:31
+ Date: 21/11/2017 16:57:55
 */
 
 SET NAMES utf8mb4;
@@ -22,48 +22,58 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `comment`;
 CREATE TABLE `comment`  (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `comment` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `published` tinyint(3) NULL DEFAULT NULL,
-  `date` datetime(0) NULL DEFAULT NULL,
+  `published` tinyint(3) UNSIGNED NOT NULL,
+  `date` datetime(0) NOT NULL,
+  `likes` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of comment
+-- ----------------------------
+INSERT INTO `comment` VALUES (1, 'Виктор', 'x-_man@mail.ru', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores culpa dignissimos enim expedita, maxime nisi porro quas quibusdam voluptate voluptatum?', 1, '2017-11-21 16:23:29', 7);
+INSERT INTO `comment` VALUES (2, 'Виктор', 'x-_man@mail.ru', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores culpa dignissimos enim expedita, maxime nisi porro quas quibusdam voluptate voluptatum?', 1, '2017-11-21 16:23:29', 14);
+INSERT INTO `comment` VALUES (3, 'Виктор', 'x-_man@mail.ru', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores culpa dignissimos enim expedita, maxime nisi porro quas quibusdam voluptate voluptatum?', 1, '2017-11-21 16:23:29', 0);
 
 -- ----------------------------
 -- Table structure for config
 -- ----------------------------
 DROP TABLE IF EXISTS `config`;
 CREATE TABLE `config`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `value` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`name`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of config
 -- ----------------------------
-INSERT INTO `config` VALUES ('onetime_training', 'Разовая тренировка: <strong>40 грн, 30 грн, 30 грн</strong> для взрослых, школьников и студентов\r\n                соотведственно.');
+INSERT INTO `config` VALUES (1, 'onetime_training', 'Разовая тренировка: <strong>40 грн, 30 грн, 30 грн</strong> для взрослых, школьников и студентов\r\n                соотведственно.');
 
 -- ----------------------------
--- Table structure for plans
+-- Table structure for plan
 -- ----------------------------
-DROP TABLE IF EXISTS `plans`;
-CREATE TABLE `plans`  (
+DROP TABLE IF EXISTS `plan`;
+CREATE TABLE `plan`  (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `price_adult` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `price_students` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `price_sсhools` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `price_schools` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of plans
+-- Records of plan
 -- ----------------------------
-INSERT INTO `plans` VALUES (1, '1 Месяц', '350 грн.', '300 грн.', '250 грн.');
-INSERT INTO `plans` VALUES (2, '3 Месяца', '750 грн.', '700 грн.', ' 650 грн.');
-INSERT INTO `plans` VALUES (3, 'Пол года', '1300 грн.', '1200 грн.', '1100 грн.');
-INSERT INTO `plans` VALUES (4, 'Год', '2400 грн.', '2300 грн.', '2200 грн.');
+INSERT INTO `plan` VALUES (1, '1 Месяц', '350 грн.', '300 грн.', '250 грн.');
+INSERT INTO `plan` VALUES (2, '3 Месяца', '750 грн.', '700 грн.', ' 650 грн.');
+INSERT INTO `plan` VALUES (3, 'Пол года', '1300 грн.', '1200 грн.', '1100 грн.');
+INSERT INTO `plan` VALUES (4, 'Год', '2400 грн.', '2300 грн.', '2200 грн.');
 
 SET FOREIGN_KEY_CHECKS = 1;
