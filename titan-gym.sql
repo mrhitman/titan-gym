b97b8a8157a025d2d3fd0e2ca0834702fc4d0870
hitman <hitman@quartsoft.com>
2017-11-23 16:51:04 +0200

    -

--- titan-gym.sql
+++ titan-gym.sql
@@ -11,11 +11,28 @@
  Target Server Version : 50720
  File Encoding         : 65001
 
- Date: 21/11/2017 16:57:55
+ Date: 23/11/2017 16:50:51
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for answer
+-- ----------------------------
+DROP TABLE IF EXISTS `answer`;
+CREATE TABLE `answer`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `body` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `comment_id` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of answer
+-- ----------------------------
+INSERT INTO `answer` VALUES (1, 'Титан', 'Ответ 1', 1);
 
 -- ----------------------------
 -- Table structure for comment
@@ -28,16 +45,8 @@ CREATE TABLE `comment`  (
   `comment` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `published` tinyint(3) UNSIGNED NOT NULL,
   `date` datetime(0) NOT NULL,
-  `likes` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of comment
--- ----------------------------
-INSERT INTO `comment` VALUES (1, 'Виктор', 'x-_man@mail.ru', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores culpa dignissimos enim expedita, maxime nisi porro quas quibusdam voluptate voluptatum?', 1, '2017-11-21 16:23:29', 7);
-INSERT INTO `comment` VALUES (2, 'Виктор', 'x-_man@mail.ru', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores culpa dignissimos enim expedita, maxime nisi porro quas quibusdam voluptate voluptatum?', 1, '2017-11-21 16:23:29', 14);
-INSERT INTO `comment` VALUES (3, 'Виктор', 'x-_man@mail.ru', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores culpa dignissimos enim expedita, maxime nisi porro quas quibusdam voluptate voluptatum?', 1, '2017-11-21 16:23:29', 0);
+) ENGINE = InnoDB AUTO_INCREMENT = 36 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for config
@@ -53,7 +62,7 @@ CREATE TABLE `config`  (
 -- ----------------------------
 -- Records of config
 -- ----------------------------
-INSERT INTO `config` VALUES (1, 'onetime_training', 'Разовая тренировка: <strong>40 грн, 30 грн, 30 грн</strong> для взрослых, школьников и студентов\r\n                соотведственно.');
+INSERT INTO `config` VALUES (1, 'onetime_training', 'Разовая тренировка: <strong>40 грн, 30 грн, 30 грн</strong> для взрослых, школьников и студентов                соотведственно.');
 
 -- ----------------------------
 -- Table structure for plan
@@ -74,6 +83,6 @@ CREATE TABLE `plan`  (
 INSERT INTO `plan` VALUES (1, '1 Месяц', '350 грн.', '300 грн.', '250 грн.');
 INSERT INTO `plan` VALUES (2, '3 Месяца', '750 грн.', '700 грн.', ' 650 грн.');
 INSERT INTO `plan` VALUES (3, 'Пол года', '1300 грн.', '1200 грн.', '1100 грн.');
-INSERT INTO `plan` VALUES (4, 'Год', '2400 грн.', '2300 грн.', '2200 грн.');
+INSERT INTO `plan` VALUES (4, 'Год', '2400 грн.', '2300 грн.', '2100 грн.');
 
 SET FOREIGN_KEY_CHECKS = 1;
